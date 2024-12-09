3d36acd39711e7d8f3defd08f76901f302240f22
Sherif Ali <105814963+sherif566@users.noreply.github.com>
2024-12-09 15:06:21 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('v1')->group(function(){
+
+    Route::apiResource('/categories',CategoriesController::class);
+    Route::apiResource('/notes',NotesController::class);
+
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
